8cb22122e57d3001d7dd1c480f61c38ee64d2599
Fernando Pires <ferjpires@gmail.com>
2025-01-03 14:22:47 +0000

    fix

--- src/main.py
+++ src/main.py
@@ -6,9 +6,6 @@ import sys
 import json
 import matplotlib.pyplot as plt
 
-from utils.graph import print_graph
-from map.mapGenerator import MapGenerator
-from simulation.simulation import Simulation
 from simulation.sim import SimulationWithLimits
 
 
